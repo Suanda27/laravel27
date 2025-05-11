ce5416425eab4ebe6830b16610da3e1bd029d6d0
Faiz_suanda <faizsuanda27@gmail.com>
2025-05-11 11:42:32 +0700

    update readme, dan penambahan file tes18.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+    Selamat datang di Home {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+</div>
